41e1ce9c03282698d168c2c5d182ce4ceb032f54
fattoliu <724684054@qq.com>
2024-10-17 11:07:02 +0800

    test: add test code.

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-#
+# This is fatto a mano.
 # Auto install Shadowsocks Server (all version)
 #
 # Copyright (C) 2016-2019 Teddysun <i@teddysun.com>
